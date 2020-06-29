3e8a3de9926723c51a6c90e309ba38c9514fade5
AnierinB <anierinb@evolution-x.org>
2020-06-29 02:19:50 +0000

    sdm845-common: init: Set dalvikvm_properties based on RAM size
    Co-Authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- init/init_sdm845.cpp
+++ init/init_sdm845.cpp
@@ -34,6 +34,7 @@
 #include "property_service.h"
 #include "vendor_init.h"
 #include <android-base/properties.h>
+#include <sys/sysinfo.h>
 
 using android::init::property_set;
 
@@ -54,15 +55,35 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
 	property_override(vendor_prop, value);
 }
 
-void vendor_load_properties()
+void load_dalvikvm_properties()
 {
-	// Common dalvik heap config
-	property_override_dual("dalvik.vm.heapstartsize", "dalvik.vm.heapstartsize", "16m");
+	struct sysinfo sys;
+
+	sysinfo(&sys);
+	if (sys.totalram < 7000ull * 1024 * 1024)
+	{
+		// 6GB RAM
+		property_override_dual("dalvik.vm.heapstartsize", "dalvik.vm.heapstartsize", "16m");
+		property_override_dual("dalvik.vm.heaptargetutilization", "dalvik.vm.heaptargetutilization", "0.5");
+		property_override_dual("dalvik.vm.heapmaxfree", "dalvik.vm.heapmaxfree", "32m");
+	}
+	else
+	{
+		// 8/10GB RAM
+		property_override_dual("dalvik.vm.heapstartsize", "dalvik.vm.heapstartsize", "24m");
+		property_override_dual("dalvik.vm.heaptargetutilization", "dalvik.vm.heaptargetutilization", "0.46");
+		property_override_dual("dalvik.vm.heapmaxfree", "dalvik.vm.heapmaxfree", "48m");
+	}
+
 	property_override_dual("dalvik.vm.heapgrowthlimit", "dalvik.vm.heapgrowthlimit", "256m");
 	property_override_dual("dalvik.vm.heapsize", "dalvik.vm.heapsize", "512m");
-	property_override_dual("dalvik.vm.heaptargetutilization", "dalvik.vm.heaptargetutilization", "0.5");
 	property_override_dual("dalvik.vm.heapminfree", "dalvik.vm.heapminfree", "8m");
-	property_override_dual("dalvik.vm.heapmaxfree", "dalvik.vm.heapmaxfree", "32m");
+}
+
+void vendor_load_properties()
+{
+	// Load dalvik config
+	load_dalvikvm_properties();
 
        // Common maintainer props
        property_override("org.evolution.build_donate_url", "https://www.paypal.com/paypalme/ChanduDyavanapelli");
