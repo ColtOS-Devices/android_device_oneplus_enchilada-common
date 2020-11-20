crdroidandroid/android_device_oneplus_sdm845-common

sdm845-common: DeviceSettings: Send broadcast on alert slider change
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: AnierinB <anierinb@evolution-x.org>
Change-Id: Idc156fb131fb05b7e0f6e9d3b522b4eaeb9e353e

DeviceSettings/src/org/lineageos/device/DeviceSettings/Constants.java
@@ -52,6 +52,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "org.lineageos.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);

ColtOS-Devices/android_device_oneplus_enchilada-common

Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/org/evolution/device/DeviceSettings/Constants.java
@@ -52,6 +52,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "org.evolution.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);