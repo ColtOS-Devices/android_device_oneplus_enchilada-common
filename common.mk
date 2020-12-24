2329a864bced2a4332bc75030e87a29c8459db10
chandu078 <chandudyavanapelli03@gmail.com>
2020-12-24 11:21:59 +0530

    sdm845-common: Remove GoogleCamera * Google hates us.
    Revert "sdm845-common: prebuilt: Add GoogleCamera"
    
    This reverts commit 07a477f758f32a3a75260c1738d2f1013535b170.
    
    Revert "sdm845-common: prebuilt: Rename GoogleCamera --> Googlecamera"
    
    This reverts commit e91e2ab52712fe7e5ea443439aa63064bcc3fb33.
    
    Revert "sdm845-common: removepackages: Nuke GoogleCamera"
    
    This reverts commit 31caafb6fa0f7e4fcaf159c8ece67a77dd8acc55.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera HIDL
 PRODUCT_PACKAGES += \
-    Googlecamera \
+    Snap \
     libcvface_api \
     libqti-perfd-client \
     vendor.oneplus.camera.CameraHIDL@1.0 \
