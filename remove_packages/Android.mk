c8f6f192b26827c13af7dba1e4fc398beea34927
chandu078 <chandudyavanapelli03@gmail.com>
2020-12-30 16:28:23 +0000

    sdm845-common: remove some more gapps
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CarrierSetup ConnMO DCMO Drive FM2 GoogleTTS Maps MyVerizonServices NfcNci OBDM_Permissions PrebuiltGmail RecorderPrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := arcore CarrierSetup ConnMO DCMO Drive FM2 FilesPrebuilt GoogleTTS libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices MaestroPrebuilt NfcNci NgaResources OBDM_Permissions PrebuiltGmail RecorderPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
