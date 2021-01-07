timmah1984/device_oneplus_sdm845-common

sdm845-common: DeviceSettings: Add LogTile * Requires root
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/org/corvus/device/DeviceSettings/LogTile.java
@@ -1,0 +1,136 @@
+package org.corvus.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.corvus.device.DeviceSettings.R;
+
+@TargetApi(24)
+public class LogTile extends TileService {
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    public Dialog logDialog() {
+        CharSequence options[] = new CharSequence[]{
+                "Logcat", "LogcatRadio", "Dmesg"};
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle(R.string.quick_settings_log_tile_dialog_title);
+        alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (SuShell.detectValidSuInPath()) {
+                    switch (which) {
+                        case 0:
+                            new CreateLogTask().execute(true, false, false);
+                            break;
+                        case 1:
+                            new CreateLogTask().execute(false, true, false);
+                            break;
+                        case 2:
+                            new CreateLogTask().execute(false, false, true);
+                            break;
+                    }
+                } else {
+                    Toast.makeText(LogTile.this,
+                            R.string.cannot_get_su, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        return alertDialog.create();
+    }
+
+    public void makeLogcat() throws SuShell.SuDeniedException, IOException {
+        final String LOGCAT_FILE = new File(Environment
+            .getExternalStorageDirectory(), "LogCat.txt").getAbsolutePath();
+        String command = "logcat -d";
+        command += " > " + LOGCAT_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    public void makeLogcatRadio() throws SuShell.SuDeniedException, IOException {
+        final String LOGCAT_RADIO_FILE = new File(Environment
+            .getExternalStorageDirectory(), "LogcatRadio.txt").getAbsolutePath();
+        String command = "logcat -d -b radio";
+        command += " > " + LOGCAT_RADIO_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    public void makeDmesg() throws SuShell.SuDeniedException, IOException {
+        final String DMESG_FILE = new File(Environment
+            .getExternalStorageDirectory(), "Dmesg.txt").getAbsolutePath();
+        String command = "dmesg -T";
+        command += " > " + DMESG_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    private class CreateLogTask extends AsyncTask<Boolean, Void, Void> {
+
+        private Exception mException = null;
+
+        @Override
+        protected Void doInBackground(Boolean... params) {
+            try {
+                if (params[0]) {
+                    makeLogcat();
+                }
+                if (params[1]) {
+                    makeLogcatRadio();
+                }
+                if (params[2]) {
+                    makeDmesg();
+                }
+            } catch (SuShell.SuDeniedException e) {
+                mException = e;
+            } catch (IOException e) {
+                e.printStackTrace();
+                mException = e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (mException instanceof SuShell.SuDeniedException) {
+                Toast.makeText(LogTile.this, R.string.cannot_get_su,
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        showDialog(logDialog());
+    }
+}

DeviceSettings/src/org/corvus/device/DeviceSettings/SuShell.java
@@ -1,0 +1,164 @@
+package org.corvus.device.DeviceSettings;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuShell {
+
+    private static final String TAG = SuShell.class.getSimpleName();
+
+    // WARNING: setting this to true will dump passwords to logcat
+    // set to false for release
+    static final boolean DEBUG = false;
+
+    private SuShell() {
+    }
+
+    public static ArrayList<String> runWithSuCheck(String... commands) throws SuDeniedException {
+        String suTestScript = "#!/system/bin/sh\necho ";
+        String suTestScriptValid = "crDroidSuPermsOk";
+
+        String[] commandsWithCheck = new String[commands.length+1];
+        commandsWithCheck[0] = suTestScript + suTestScriptValid;
+        System.arraycopy(commands, 0, commandsWithCheck, 1, commands.length);
+
+        ArrayList<String> output = runWithSu(commandsWithCheck);
+        if (output.size() >= 1
+                && output.get(0).trim().equals(suTestScriptValid)) {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth confirmed");
+            }
+            output.remove(0);
+            return output;
+        } else {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth refused");
+            }
+            throw new SuDeniedException();
+        }
+    }
+
+    public static ArrayList<String> runWithShell(String... command) {
+        return run("/system/bin/sh", command);
+    }
+
+    public static ArrayList<String> runWithSu(String... command) {
+        return run("su", command);
+    }
+
+    public static ArrayList<String> run(String shell, ArrayList<String> commands) {
+        String[] commandsArray = new String[commands.size()];
+        commands.toArray(commandsArray);
+        return run(shell, commandsArray);
+    }
+
+    public static ArrayList<String> run(String shell, String... commands) {
+        ArrayList<String> output = new ArrayList<String>();
+
+        try {
+            Process process = Runtime.getRuntime().exec(shell);
+
+            BufferedOutputStream shellInput = new BufferedOutputStream(
+                    process.getOutputStream());
+            BufferedReader shellOutput = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            for (String command : commands) {
+                if (DEBUG) {
+                    Log.i(TAG, "command: " + command);
+                }
+                shellInput.write((command + " 2>&1\n").getBytes());
+            }
+
+            shellInput.write("exit\n".getBytes());
+            shellInput.flush();
+
+            String line;
+            while ((line = shellOutput.readLine()) != null) {
+                if (DEBUG) {
+                    Log.d(TAG, "command output: " + line);
+                }
+                output.add(line);
+            }
+
+            process.waitFor();
+        } catch (IOException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static String getCommandOutput(String command)
+            throws IOException {
+
+        StringBuilder output = new StringBuilder();
+
+        if (DEBUG) {
+            Log.d(TAG, "Getting output for command: " + command);
+        }
+        Process p = Runtime.getRuntime().exec(command);
+        InputStream is = p.getInputStream();
+        InputStreamReader r = new InputStreamReader(is);
+        BufferedReader in = new BufferedReader(r);
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            output.append(line);
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    public static boolean detectValidSuInPath() {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File su = new File(path + "/su");
+            if (su.exists()) {
+                // should check if it is suid
+                if (DEBUG) {
+                    Log.d(TAG, "Found su at " + su.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean findInPath(String cmd) {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File cmdFile = new File(path, cmd);
+            if (cmdFile.exists()) {
+                if (DEBUG) {
+                    Log.d(TAG, "Found " + cmd + " at " + cmdFile.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static class SuDeniedException extends Exception {};
+}

DeviceSettings/src/org/corvus/device/DeviceSettings/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.corvus.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.corvus.device.DeviceSettings.R;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}

ColtOS-Devices/android_device_oneplus_enchilada-common

DeviceSettings/src/org/evolution/device/DeviceSettings/LogTile.java
@@ -1,0 +1,136 @@
+package org.evolution.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.evolution.device.DeviceSettings.R;
+
+@TargetApi(24)
+public class LogTile extends TileService {
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    public Dialog logDialog() {
+        CharSequence options[] = new CharSequence[]{
+                "Logcat", "LogcatRadio", "Dmesg"};
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle(R.string.quick_settings_log_tile_dialog_title);
+        alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (SuShell.detectValidSuInPath()) {
+                    switch (which) {
+                        case 0:
+                            new CreateLogTask().execute(true, false, false);
+                            break;
+                        case 1:
+                            new CreateLogTask().execute(false, true, false);
+                            break;
+                        case 2:
+                            new CreateLogTask().execute(false, false, true);
+                            break;
+                    }
+                } else {
+                    Toast.makeText(LogTile.this,
+                            R.string.cannot_get_su, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        return alertDialog.create();
+    }
+
+    public void makeLogcat() throws SuShell.SuDeniedException, IOException {
+        final String LOGCAT_FILE = new File(Environment
+            .getExternalStorageDirectory(), "LogCat.txt").getAbsolutePath();
+        String command = "logcat -d";
+        command += " > " + LOGCAT_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    public void makeLogcatRadio() throws SuShell.SuDeniedException, IOException {
+        final String LOGCAT_RADIO_FILE = new File(Environment
+            .getExternalStorageDirectory(), "LogcatRadio.txt").getAbsolutePath();
+        String command = "logcat -d -b radio";
+        command += " > " + LOGCAT_RADIO_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    public void makeDmesg() throws SuShell.SuDeniedException, IOException {
+        final String DMESG_FILE = new File(Environment
+            .getExternalStorageDirectory(), "Dmesg.txt").getAbsolutePath();
+        String command = "dmesg -T";
+        command += " > " + DMESG_FILE;
+        SuShell.runWithSuCheck(command);
+    }
+
+    private class CreateLogTask extends AsyncTask<Boolean, Void, Void> {
+
+        private Exception mException = null;
+
+        @Override
+        protected Void doInBackground(Boolean... params) {
+            try {
+                if (params[0]) {
+                    makeLogcat();
+                }
+                if (params[1]) {
+                    makeLogcatRadio();
+                }
+                if (params[2]) {
+                    makeDmesg();
+                }
+            } catch (SuShell.SuDeniedException e) {
+                mException = e;
+            } catch (IOException e) {
+                e.printStackTrace();
+                mException = e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (mException instanceof SuShell.SuDeniedException) {
+                Toast.makeText(LogTile.this, R.string.cannot_get_su,
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        showDialog(logDialog());
+    }
+}

DeviceSettings/src/org/evolution/device/DeviceSettings/SuShell.java
@@ -1,0 +1,164 @@
+package org.evolution.device.DeviceSettings;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuShell {
+
+    private static final String TAG = SuShell.class.getSimpleName();
+
+    // WARNING: setting this to true will dump passwords to logcat
+    // set to false for release
+    static final boolean DEBUG = false;
+
+    private SuShell() {
+    }
+
+    public static ArrayList<String> runWithSuCheck(String... commands) throws SuDeniedException {
+        String suTestScript = "#!/system/bin/sh\necho ";
+        String suTestScriptValid = "crDroidSuPermsOk";
+
+        String[] commandsWithCheck = new String[commands.length+1];
+        commandsWithCheck[0] = suTestScript + suTestScriptValid;
+        System.arraycopy(commands, 0, commandsWithCheck, 1, commands.length);
+
+        ArrayList<String> output = runWithSu(commandsWithCheck);
+        if (output.size() >= 1
+                && output.get(0).trim().equals(suTestScriptValid)) {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth confirmed");
+            }
+            output.remove(0);
+            return output;
+        } else {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth refused");
+            }
+            throw new SuDeniedException();
+        }
+    }
+
+    public static ArrayList<String> runWithShell(String... command) {
+        return run("/system/bin/sh", command);
+    }
+
+    public static ArrayList<String> runWithSu(String... command) {
+        return run("su", command);
+    }
+
+    public static ArrayList<String> run(String shell, ArrayList<String> commands) {
+        String[] commandsArray = new String[commands.size()];
+        commands.toArray(commandsArray);
+        return run(shell, commandsArray);
+    }
+
+    public static ArrayList<String> run(String shell, String... commands) {
+        ArrayList<String> output = new ArrayList<String>();
+
+        try {
+            Process process = Runtime.getRuntime().exec(shell);
+
+            BufferedOutputStream shellInput = new BufferedOutputStream(
+                    process.getOutputStream());
+            BufferedReader shellOutput = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            for (String command : commands) {
+                if (DEBUG) {
+                    Log.i(TAG, "command: " + command);
+                }
+                shellInput.write((command + " 2>&1\n").getBytes());
+            }
+
+            shellInput.write("exit\n".getBytes());
+            shellInput.flush();
+
+            String line;
+            while ((line = shellOutput.readLine()) != null) {
+                if (DEBUG) {
+                    Log.d(TAG, "command output: " + line);
+                }
+                output.add(line);
+            }
+
+            process.waitFor();
+        } catch (IOException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static String getCommandOutput(String command)
+            throws IOException {
+
+        StringBuilder output = new StringBuilder();
+
+        if (DEBUG) {
+            Log.d(TAG, "Getting output for command: " + command);
+        }
+        Process p = Runtime.getRuntime().exec(command);
+        InputStream is = p.getInputStream();
+        InputStreamReader r = new InputStreamReader(is);
+        BufferedReader in = new BufferedReader(r);
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            output.append(line);
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    public static boolean detectValidSuInPath() {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File su = new File(path + "/su");
+            if (su.exists()) {
+                // should check if it is suid
+                if (DEBUG) {
+                    Log.d(TAG, "Found su at " + su.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean findInPath(String cmd) {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File cmdFile = new File(path, cmd);
+            if (cmdFile.exists()) {
+                if (DEBUG) {
+                    Log.d(TAG, "Found " + cmd + " at " + cmdFile.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static class SuDeniedException extends Exception {};
+}

DeviceSettings/src/org/evolution/device/DeviceSettings/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.evolution.device.DeviceSettings.R;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}