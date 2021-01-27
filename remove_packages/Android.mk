e3561371f73612402af60aa9663f650450d67af4
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-27 11:05:28 +0000

    sdm845-common: Removepackages: Don't Build OnePlus Apps * OnePlusCamera * OnePlusGallery * Build WallpapersBReel2020 Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore CarrierSetup ConnMO DCMO Drive FM2 FilesPrebuilt GoogleTTS libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices MaestroPrebuilt NfcNci NgaResources OBDM_Permissions PrebuiltGmail RecorderPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := arcore CarrierSetup ConnMO DCMO Drive FM2 FilesPrebuilt GoogleTTS libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices MaestroPrebuilt NfcNci NgaResources OnePlusCamera OnePlusGallery OBDM_Permissions PrebuiltGmail RecorderPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
