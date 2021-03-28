7ccdeaf32725117d0a9ecb28fc7d4ee9bfce2b61
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-03-28 12:11:41 +0530

    sdm845-common: Disable soundtrigger via vendor overlay

--- common.mk
+++ common.mk
@@ -251,6 +251,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
+# Vendor Overlays
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vendor-overlay/etc/vintf/manifest/st_disable.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest/st_disable.xml
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
